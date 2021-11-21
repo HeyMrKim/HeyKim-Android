7559a230cd6351b24d3bae18b9a846224ae0126f
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2021-11-21 18:05:49 +0900

    feat : 서버에서 받을 데이터

---
+++ app/src/main/java/com/example/neeraj/ticketsnew/LoginResponse.java
@@ -0,0 +1,40 @@
+package com.example.neeraj.ticketsnew;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResponse {
+
+    @SerializedName("resultCode")
+    public String resultCode;
+
+    @SerializedName("access_token")
+    public String token;
+
+    //@SerializedName("staff")
+    //public String staff;
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+/*
+    public String getStaff() {
+        return staff;
+    }
+
+    public void setStaff(String staff) {
+        this.staff = staff;
+    }
+ */
+}
